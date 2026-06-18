b817a501cb6847928dd81a652762f31e093a261f
Marina <ariamarina0@gmail.com>
2026-06-18 10:44:05 +0300

    TEMPORARILY DISABLED MIGRATIONS AT STARTUP

--- backend/Program.cs
+++ backend/Program.cs
@@ -38,10 +38,4 @@ app.UseCors("AllowFrontend");
 
 app.MapControllers();
 
-using (var scope = app.Services.CreateScope())
-{
-    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    db.Database.Migrate();
-}
-
 app.Run();
