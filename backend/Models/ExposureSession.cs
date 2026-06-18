e80d8ea7efa8b011e8078a95e5b82bde27dc402f
Marina <ariamarina0@gmail.com>
2026-06-18 11:32:36 +0300

    Sync ef model for PostgreSQL

--- backend/Models/ExposureSession.cs
+++ backend/Models/ExposureSession.cs
@@ -4,7 +4,7 @@ public class ExposureSession
 {
     public int Id{get;set;}
     public int FearId{get;set;}
-    public Fear Fear{get;set;} = null;
+    public Fear Fear{get;set;} = null!;
     public DateTime Date{get;set;} = DateTime.UtcNow;
     public int AnxietyBefore{get;set;}
     public int AnxietyAfter{get;set;}
