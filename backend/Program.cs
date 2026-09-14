eac770978d40d6b879b018be567fa178d7c46e39
Marina <ariamarina0@gmail.com>
2026-09-15 01:01:37 +0300

    Remove automatic database migration on startup

--- backend/Program.cs
+++ backend/Program.cs
@@ -33,10 +33,4 @@ app.MapControllers();
 
 app.MapGet("/", () => "Anxiety Exposure Tracker API is running");
 
-using (var scope = app.Services.CreateScope())
-{
-    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    db.Database.Migrate();
-}
-
 app.Run();
